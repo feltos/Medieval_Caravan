c3dce252ad774b78f4f1cb960d2f804bd8b33adb
guillaume volgyesi <guillaumevolgyesi@gmail.com>
2017-12-12 14:34:00 +0100

    build map and roads

---
+++ Assets/Scripts/TownsManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TownsManager : MonoBehaviour
+{
+    Vector3 baseScale;
+    Vector3 highlightScale;
+
+    [SerializeField]
+    Canvas townInterface;
+    [SerializeField]
+    GameObject map;
+    GameObject player;
+
+	void Start ()
+    {
+        baseScale = transform.localScale;
+        highlightScale = transform.localScale + new Vector3(0.1f, 0.1f, 0.1f);
+        player = GameObject.Find("Player");
+	}
+
+    private void OnMouseOver()
+    {
+        transform.localScale = highlightScale;
+    }
+
+    private void OnMouseExit()
+    {
+        transform.localScale = baseScale;
+    }
+
+    private void OnMouseDown()
+    {
+        if(player.transform.parent == gameObject.transform)
+        {
+            townInterface.gameObject.SetActive(true);
+            map.SetActive(false);
+        }  
+    }
+}
